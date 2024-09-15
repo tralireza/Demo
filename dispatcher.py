b7471de5bd69be1f8df73b602352b4e17a3c1420
Alireza <26859725+tralireza@users.noreply.github.com>
2024-09-16 00:05:06 +0100

    dispatcher update, env.appConfig fixture for test

--- dispatcher.py
+++ dispatcher.py
@@ -3,7 +3,7 @@ import lxml.etree as et
 from ncclient import manager
 from ncclient.operations import RPCError
 
-ncMgr, resetMgr = None, None
+mgr, reset = None, None
 logger = logging.getLogger(__name__)
 
 
@@ -14,54 +14,51 @@ def init(appConfig):
     return True if connection to remote host is successful,
     otherwise False to fast-fail
     """
-    global resetMgr
+    global reset
 
-    def resetMgr():
+    def reset():
         """
         closure on appConfig to reset/retry ncMgr in case of connection loss
         """
-        global ncMgr
+        global mgr
 
-        ncMgr = None
+        mgr = None
         try:
-            ncMgr = manager.connect(port=830, timeout=90,
-                                    host=appConfig['HOST'],
-                                    username=appConfig['USR'], password=appConfig['PASSWD'],
-                                    hostkey_verify=False, allow_agent=False,
-                                    device_params={'name': 'iosxr'})
+            mgr = manager.connect(port=830, timeout=90,
+                                  host=appConfig['HOST'],
+                                  username=appConfig['USR'],
+                                  password=appConfig['PASSWD'],
+                                  hostkey_verify=False, allow_agent=False,
+                                  device_params={'name': 'iosxr'})
+
             logger.info('connected to "%s"' % appConfig['HOST'])
+
         except Exception as e:
             logger.error(e)
 
-        return ncMgr is not None
+        return mgr is not None
 
-    return resetMgr()
+    return reset()
 
 
 def dispatch(rpcXml):
     """
     send the rpc payload (sync), reset connection manager (ncMgr) on Exception
     """
-    logger.debug('+dispatching RCP paylad: %s' % rpcXml)
+    logger.debug('dispatching RCP paylad -> %s' % rpcXml)
     try:
-        rspXml = ncMgr.dispatch(et.fromstring(rpcXml)).xml
+        rspXml = mgr.dispatch(et.fromstring(rpcXml)).xml
         return True
     except RPCError as e:
         rspXml = e.xml
-    except Exception:
+    except Exception as e:
+        logger.error(e)
         logger.info('trying to re-connect to remote host ...')
-        resetMgr()
+        reset()
         raise Exception()
 
     if et.iselement(rspXml):
         rspXml = et.tostring(rspXml, pretty_print=True).decode()
 
-    rspOutput = ''
-    try:
-        rspOutput = et.tostring(et.fromstring(rspXml.encode('utf-8')),
-                                pretty_print=True).decode()
-    except Exception as e:
-        logger.error(e)
-
-    logger.info(rspOutput)
+    logger.debug('RPC response -> %s' % rspXml)
     return False
--- dispatcher_test.py
+++ dispatcher_test.py
@@ -1,32 +1,31 @@
-import dispatcher as m
 import logging
+
 import pytest
-import os
+
+import env
+import dispatcher as m
+
+
+m.logger.setLevel(logging.DEBUG)
 
 
 @pytest.fixture()
 def appConfig():
-    appConfig = {
-         'HOST': os.getenv('HOST', '127.0.0.1'),
-         'USR': os.getenv('USR', ''),
-         'PASSWD': os.getenv('PASSWD', ''),
-         }
-
+    appConfig = env.appConfig
     yield appConfig
 
 
-def test_connToRemote(appConfig):
+def test_connect(appConfig):
     host = appConfig['HOST']
 
     appConfig['HOST'] = '127.0.0.1'
-    assert m.init(appConfig) == False
+    assert m.init(appConfig) is False
 
     appConfig['HOST'] = host
-    assert m.init(appConfig) == True
+    assert m.init(appConfig) is True
 
 
 def test_dispatch(appConfig):
-    m.logger.setLevel(logging.DEBUG)
 
     m.init(appConfig)
     assert m.dispatch('''
@@ -43,4 +42,4 @@ def test_dispatch(appConfig):
     </interfaces>
   </config>
 </edit-config>
-''' % (1234, 9191)) == True
+''' % (1234, 9191)) is True
